55f2ef19a0c0acc085dbd0b109eae2a684a637aa
technotablet <subs.vk@exain.com>
2024-12-21 17:09:00 +0530

    Pinned Highcharts version to v10
    Highcharts 12 in Dec 2024 was giving trouble. Pinned it to an older version.

--- data.php
+++ data.php
@@ -47,8 +47,8 @@ if (!isset($_SESSION['fb_client_id']) || $_SESSION['fb_client_id'] == '') {
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-		<script src='https://code.highcharts.com/highcharts.js'></script>
-		<script src='https://code.highcharts.com/modules/exporting.js'></script>
+		<script src='https://code.highcharts.com/10/highcharts.js'></script>
+		<script src='https://code.highcharts.com/10/modules/exporting.js'></script>
 
 		<!-- date picker -->
 		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker3.css">
